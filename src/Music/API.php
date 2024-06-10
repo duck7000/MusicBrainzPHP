ff471ddb78645e29045d5c82b5f817f2f4c587f2
duck7000 <ed@riethorst.net>
2024-06-10 17:29:50 +0200

    API class added methods for TitleSearchAdvanced

--- src/Music/API.php
+++ src/Music/API.php
@@ -41,13 +41,13 @@ class Api
     }
 
     /**
-     * Search request
+     * Search request for TitleSearch class
      * @param string $urlSuffix
      * @return \stdClass
      */
     public function doSearch($urlSuffix)
     {
-        $baseUrl = $this->config->baseUrl;
+        $baseUrl = 'https://musicbrainz.org/ws/2/release/';
         $incUrl = '%20AND%20format:' . $this->config->titleSearchFormat .
                   '&limit=' . $this->config->titleSearchAmount .
                   '&fmt=json';
@@ -56,33 +56,113 @@ class Api
     }
 
     /**
-     * Lookup request
+     * Search for specific artist name in TitleSearchAdvanced class, this is not the same as normal artist search in TitleSearch class!
+     * @param string $urlSuffix
+     * @return \stdClass
+     */
+    public function doArtistSearch($urlSuffix)
+    {
+        $baseArtistUrl = 'https://musicbrainz.org/ws/2/artist/';
+        $incUrl = '&limit=25' .
+                  '&fmt=json';
+        $url = $baseArtistUrl . $urlSuffix . $incUrl;
+        return $this->execRequest($url);
+    }
+
+    /**
+     * Search for all releasegroups of specific artistId in TitleSearchAdvanced class
+     * @param string $artistId
+     * @param string $type Include only this type in search, exclude all others
+     * values for $type:
+     *      album
+     *      single
+     *      ep
+     *      broadcast
+     *      other
+     *      live
+     *      compilation
+     *      remix
+     *      interview
+     *      soundtrack
+     *      spokenword
+     *      audiobook
+     *      demo
+     * @return \stdClass
+     */
+    public function doReleaseGroupSearch($artistId, $type)
+    {
+        $releaseTypes = array(
+            "primarytype:album",
+            "primarytype:single",
+            "primarytype:ep",
+            "primarytype:broadcast",
+            "primarytype:other",
+            "secondarytype:live",
+            "secondarytype:compilation",
+            "secondarytype:remix",
+            "secondarytype:interview",
+            "secondarytype:soundtrack",
+            "secondarytype:spokenword",
+            "secondarytype:audiobook",
+            "secondarytype:demo"
+        );
+        $incUrl = '';
+        foreach ($releaseTypes as $releaseType) {
+            if (stripos($releaseType, $type) !== false) {
+                $incUrl .= '%20AND%20' . $releaseType;
+            } else {
+                $incUrl .= '%20NOT%20' . $releaseType;
+            }
+        }
+        $incUrl .= '&limit=100&fmt=json';
+        $baseUrl = 'https://musicbrainz.org/ws/2/release-group?query=arid:';
+        $url = $baseUrl . $artistId . $incUrl;
+        return $this->execRequest($url);
+    }
+
+    /**
+     * Search for all releases inside releasegroup id in TitleSearchAdvanced class
+     * @param string $relGroupId
+     * @return \stdClass
+     */
+    public function doReleaseGroupReleases($relGroupId)
+    {
+        $baseRelGroupUrl = 'https://musicbrainz.org/ws/2/release?query=rgid:';
+        $incUrl = '&fmt=json';
+        $url = $baseRelGroupUrl . $relGroupId . $incUrl;
+        return $this->execRequest($url);
+    }
+
+    /**
+     * Lookup request in Title class
      * @param string $mbID
      * @return \stdClass
      */
     public function doLookup($mbID)
     {
-        $baseUrl = $this->config->baseUrl;
+        $baseUrl = 'https://musicbrainz.org/ws/2/release/';
         $incUrl = '?inc=artist-credits' .
                         '+labels' .
                         '+discids' .
                         '+recordings' .
                         '+release-groups' .
                         '+genres' .
+                        '+tags' .
                         '+url-rels' .
+                        '+annotation' .
                         '&fmt=json';
         $url = $baseUrl . $mbID . $incUrl;
         return $this->execRequest($url);
     }
     
     /**
-     * Cover art lookup
+     * Cover art lookup in Title Class
      * @param string $mbID
      * @return \stdClass
      */
     public function doCoverArtLookup($mbID)
     {
-        $url = $this->config->baseCoverArtUrl. $mbID . '/index.json';
+        $url = 'https://ia600903.us.archive.org/6/items/mbid-' . $mbID . '/index.json';
         return $this->execRequest($url);
     }
 
