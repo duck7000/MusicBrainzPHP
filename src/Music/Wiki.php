c292e1ec52c82176a76be7a497938037a52d5a4d
duck7000 <ed@riethorst.net>
2025-01-11 22:21:41 +0100

    wiki class fixed bug personnel not been added

--- src/Music/Wiki.php
+++ src/Music/Wiki.php
@@ -225,7 +225,7 @@ class Wiki extends MdbBase
                         }
                     }
                     // personel
-                    if ($elementId == "personnel") {
+                    if ($elementId == "Personnel") {
                         if (!empty(trim(strip_tags($node->nodeValue))) &&
                             ($node->nodeName === 'ul' || $node->nodeName === 'div')
                            )
