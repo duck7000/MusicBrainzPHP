8b2e43e95979e1a444d27c421c45cff65cbe68bb
duck7000 <ed@riethorst.net>
2025-03-04 12:53:59 +0100

    Fixed php 8.4 warnings Title class construct

--- src/Music/Title.php
+++ src/Music/Title.php
@@ -53,7 +53,7 @@ class Title extends MdbBase
      * @param Config $config OPTIONAL override default config
      * @param CacheInterface $cache OPTIONAL override the default cache with any PSR-16 cache.
      */
-    public function __construct($id, Config $config = null, LoggerInterface $logger = null, CacheInterface $cache = null)
+    public function __construct(string $id, ?Config $config = null, ?LoggerInterface $logger = null, ?CacheInterface $cache = null)
     {
         parent::__construct($config, $logger, $cache);
         $this->setid($id);
