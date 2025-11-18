6f1250b625e055601ef477a4f16ad907a88bd30a
duck7000 <ed@riethorst.net>
2025-11-18 15:09:35 +0100

    Improved Lyric finding

--- src/Music/Lyric.php
+++ src/Music/Lyric.php
@@ -1,4 +1,5 @@
 <?php
+
 #############################################################################
 # musicBrainzPHP                                ed (github user: duck7000)  #
 # written by ed (github user: duck7000)                                     #
@@ -51,7 +52,6 @@ class Lyric extends MdbBase
      * @param string $albumTitle release album title
      * @param string $trackArtist track artist
      * @param string $trackName track name
-     * @param string $trackId track id number from MusicBrainz
      * @param string $trackLength track length (in seconds)
      * @return string lyric text or false
      */
@@ -59,27 +59,42 @@ class Lyric extends MdbBase
         $albumTitle,
         $trackArtist,
         $trackName,
-        $trackId,
         $trackLength)
     {
-        if (!empty($albumTitle) && !empty($trackArtist) && !empty($trackName)) {
-            $albumTitle = urlencode($albumTitle);
+        if (!empty($trackArtist) && !empty($trackName)) {
             $trackArtist = urlencode($trackArtist);
             $trackName = urlencode($trackName);
             $url = $this->lrclibApiUrl .
                    'artist_name=' . $trackArtist .
-                   '&track_name=' . $trackName .
-                   '&album_name=' . $albumTitle;
+                   '&track_name=' . $trackName;
+            if (!empty($albumTitle)) {
+                $url .= '&album_name=' . urlencode($albumTitle);
+            }
             if (!empty($trackLength)) {
                 $url .= '&duration=' . $trackLength;
             }
-            $results = $this->exactMatchApiCall($trackId, $url);
+
+            // First API call with all available parameters
+            $results = $this->exactMatchApiCall($url);
             if ($results !== false) {
                 return $results;
             }
+
+            // If false, second API call with artist and trackname
+            $urlNoAlbum = $this->lrclibApiUrl .
+                          'artist_name=' . $trackArtist .
+                          '&track_name=' . $trackName;
+            $resultsNoAlbum = $this->exactMatchApiCall($urlNoAlbum);
+            if ($resultsNoAlbum !== false) {
+                return $resultsNoAlbum;
+            }
+
+            // if still false third API call for a search
             if ($this->config->apiSearch === true) {
-                $searchUrl = $this->lrclibApiSearchUrl . 'track_name=' . $trackName . '&artist_name=' . $trackArtist;
-                $searchResults = $this->searchApiCall($trackId, $searchUrl);
+                $searchUrl = $this->lrclibApiSearchUrl .
+                             'track_name=' . $trackName .
+                             '&artist_name=' . $trackArtist;
+                $searchResults = $this->searchApiCall($searchUrl);
                 if ($searchResults !== false) {
                     return $searchResults;
                 }
@@ -104,13 +119,12 @@ class Lyric extends MdbBase
 
     /**
      * Get and process api call data
-     * @param string $trackId Id tracknumber from MusicBrainz
      * @param string $url api call url
      * @return string if lyrics data found, false otherwise
      */
-    private function exactMatchApiCall($trackId, $url)
+    private function exactMatchApiCall($url)
     {
-        $data = $this->api->checkCache($trackId, $url, "title", "_Lyric");
+        $data = $this->api->execRequest($url);
         if (isset($data->plainLyrics) && $data->plainLyrics != '') {
             return $this->removeCensoring($data->plainLyrics);
         } elseif (isset($data->instrumental) && $data->instrumental === true) {
@@ -121,13 +135,12 @@ class Lyric extends MdbBase
 
     /**
      * Get and process api search call data (if there is no exact match)
-     * @param string $trackId Id tracknumber from MusicBrainz
      * @param string $searchUrl api call search url
      * @return string if lyrics data found, false otherwise
      */
-    private function searchApiCall($trackId, $searchUrl)
+    private function searchApiCall($searchUrl)
     {
-        $searchData = $this->api->checkCache($trackId, $searchUrl, "title", "_Lyric");
+        $searchData = $this->api->execRequest($searchUrl);
         if (is_array($searchData) && count($searchData) > 0) {
             if (isset($searchData[0]->plainLyrics) && $searchData[0]->plainLyrics != '') {
                 return $this->removeCensoring($searchData[0]->plainLyrics);
